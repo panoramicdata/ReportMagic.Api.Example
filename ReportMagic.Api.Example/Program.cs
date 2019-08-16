9873714259553870220cb17117a2ae329a931abc
Peter Acs <peter.acs@panoramic.systems>
2019-08-16 15:38:10 +0100

    Initial example.

--- ReportMagic.Api.Example/Program.cs
+++ ReportMagic.Api.Example/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PanoramicData.ReportMagic.Data;
+using PanoramicData.ReportMagic.Data.RemoteSystems;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace ReportMagic.Api.Example
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async System.Threading.Tasks.Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var reportMagicClientOptions = JsonConvert.DeserializeObject<ReportMagicClientOptions>(
+				File.ReadAllText("config.json"),
+				new JsonSerializerSettings { MissingMemberHandling = MissingMemberHandling.Error }
+				);
+
+			using (var client = new ReportMagicClient(reportMagicClientOptions))
+			{
+				var schedules = (await client.GetAllAsync<ReportSchedule>().ConfigureAwait(false)).ToList();
+				Console.WriteLine($"Found {schedules.Count} schedules.");
+
+				var reportSchedule = await client.CreateAsync(
+					new ReportSchedule
+					{
+						Name = "ReportSchedule1",
+						RemoteSystemProtocol = RemoteSystemProtocol.File,
+						RemoteSystemInputPath = "/",
+						RemoteSystemOutputPath = "/",
+						CronSchedule = "0 0 0 * * ?",
+						IsEnabled = false,
+						DocxOutputIsEnabled = false,
+						PdfOutputIsEnabled = false,
+						HtmlOutputIsEnabled = false,
+						XlsxOutputIsEnabled = false,
+						ReportScheduleType = ReportScheduleType.HtmlDocumentJob,
+						LastReportBatchJobExecutionResult = ExecutionResult.NeverRun,
+						DatabaseOutputIsEnabled = false,
+						OutputToMonthlySubFolder = true,
+						RenderOption = RenderOption.Default,
+						IsDeleted = false
+					}
+				).ConfigureAwait(false);
+				Console.WriteLine($"A new reportSchedule has been created with the id: {reportSchedule.Id}");
+
+				schedules = (await client.GetAllAsync<ReportSchedule>().ConfigureAwait(false)).ToList();
+				Console.WriteLine($"Found {schedules.Count} schedules.");
+			}
 		}
 	}
 }
---
+++ ReportMagic.Api.Example/README.md
@@ -0,0 +1,10 @@
+﻿# ReportMagic API Examples
+
+Include the NuGet Package at the Package Manager Console
+
+```
+Install-Package ReportMagic.Api
+```
+
+https://www.nuget.org/packages/ReportMagic.Api/
+
